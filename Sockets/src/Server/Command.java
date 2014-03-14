ae3ec38776ea063c5c7406ff640485408ff1e07f
Milan <Milan@10.43.75.10>
2014-03-14 12:02:03 +0100

    ezeze

--- Sockets/src/Server/Command.java
+++ Sockets/src/Server/Command.java
@@ -1,6 +1,7 @@
 package Server;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -10,10 +11,13 @@ public class Command {
 		if(command.equals("HEAD")) {
 			doHeadCommand(dir);
 		}
+		if(command.equals("GET")) {
+			doGetCommand(dir);
+		}
+		if(command.equals("PUT")) {
+			doPutCommand(dir, newFile);
+		}
 	}
-
-
-
 
 	private void doHeadCommand(String dir) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("C:/Users/Milan/git/-ComputerEvilNetworks/Sockets/src/Server/" + dir));
@@ -34,8 +38,24 @@ public class Command {
 		br.close();
 	}
 
+	
+	private void doGetCommand(String dir) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("C:/Users/Milan/git/-ComputerEvilNetworks/Sockets/src/Server/" + dir));
+		String line;
+		while ((line = br.readLine()) != null) {			
+			Server.print(line);
+		}
+		br.close();
+	}
+	
+	private void doPutCommand(String dir, String newFile) {
+		File temp = new File("C:/Users/Milan/git/-ComputerEvilNetworks/Sockets/src/Server/" + newFile);
+		
+		if(temp.exists())
+		  temp.delete();
+	}
 
-
+	
 
 
 
